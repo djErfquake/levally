72df66b40b6506ed1e09ff1c31807b913ea1ffb1
Calvin LeVally <clevally@astutesolutions.com>
2021-03-04 14:18:46 -0500

    added blue alley monsters

--- src/rpg/monsters.js
+++ src/rpg/monsters.js
@@ -1,170 +1,45 @@
 module.exports = {
-    apprenticeWizard: {
-        name: "Apprentice Wizard",
-        link: "https://5e.tools/bestiary.html#apprentice%20wizard_vgm",
-        hp: "2d8",
-        dex: 0,
-        group: 'Humanoid'
-    },
-    bandit: {
-        name: "Bandit",
-        link: "https://5e.tools/bestiary.html#bandit_mm",
-        hp: "2d8+2",
-        dex: 1,
-        group: 'Humanoid'
-    },
-    deepGnome: {
-        name: "Deep Gnome",
-        link: "https://5e.tools/bestiary.html#deep%20gnome%20(svirfneblin)_mm",
-        hp: "3d6+6",
-        dex: 2,
-        group: 'Humanoid'
-    },
-    doppelganger: {
-        name: "DoppelGanger",
-        link: "https://5e.tools/bestiary.html#doppelganger_mm",
-        hp: "8d8+16",
-        dex: 4,
-        group: 'Humanoid'
-    },
-    drowGunslinger: {
-        name: "Drow Gunslinger",
-        link: "https://5e.tools/bestiary.html#drow%20gunslinger_wdh",
-        hp: "13d8+26",
-        dex: 4,
-        group: 'Humanoid'
-    },
-    duergar: {
-        name: "Duergar",
-        link: "https://5e.tools/bestiary.html#duergar_mm",
-        hp: "4d8+8",
-        dex: 0,
-        group: 'Humanoid'
-    },
-    gazer: {
-        name: "Gazer",
-        link: "https://5e.tools/bestiary.html#gazer_vgm",
-        hp: "3d4+6",
-        dex: 3,
-        group: 'Monster'
-    },
-    goblin: {
-        name: "Goblin",
-        link: "https://5e.tools/bestiary.html#goblin_mm",
-        hp: "2d6",
-        dex: 2,
-        group: 'Monster'
-    },
-    grayOoze: {
-        name: "Gray Ooze",
-        link: "https://5e.tools/bestiary.html#gray%20ooze_mm",
-        hp: "3d8+9",
-        dex: -2,
-        group: 'Monster'
-    },
-    guard: {
-        name: "Guard",
-        link: "https://5e.tools/bestiary.html#guard_mm",
-        hp: "2d8+2",
-        dex: 1,
-        group: 'Humanoid'
-    },
-    intellectDevourer: {
-        name: "Intellect Devourer",
-        link: "https://5e.tools/bestiary.html#intellect%20devourer_mm",
-        hp: "6d4+5",
-        dex: 2,
-        group: 'Monster'
-    },
-    kenku: {
-        name: "Kenku",
-        link: "https://5e.tools/bestiary.html#kenku_mm",
-        hp: "3d8",
-        dex: 3,
-        group: 'Monster'
-    },
-    rat: {
-        name: "Rat",
-        link: "https://5e.tools/bestiary.html#rat_mm",
-        hp: "1d4-1",
-        dex: 0,
-        group: 'Beast'
-    },
-    stirges: {
-        name: "Stirge",
-        link: "https://5e.tools/bestiary.html#stirge_mm",
-        hp: "1d4",
-        dex: 3,
-        group: 'Beast'
-    },
-    swashbuckler: {
-        name: "Swashbuckler",
-        link: "https://5e.tools/bestiary.html#swashbuckler_vgm",
-        hp: "12d8+12",
-        dex: 4,
-        group: 'Humanoid'
-    },
-    troll: {
-        name: "Troll",
-        link: "https://5e.tools/bestiary.html#troll_mm",
-        hp: "8d10+40",
-        dex: 1,
-        group: 'Monster'
-    },
-    veteran: {
-        name: "Veteran",
-        link: "https://5e.tools/bestiary.html#veteran_mm",
-        hp: "9d8+18",
-        dex: 1,
-        group: 'Humanoid'
-    },
-    wererat: {
-        name: "Wererat",
-        link: "https://5e.tools/bestiary.html#wererat_mm",
-        hp: "6d8+6",
-        dex: 2,
-        group: 'Monster'
-    },
-    victoroCassalanter: {
-        name: "Victoro Cassalanter",
-        link: "https://5e.tools/bestiary.html#victoro%20cassalanter_wdh",
-        hp: "15d8+30",
-        dex: 1,
-        group: 'Character'
-    },
-    ammaliaCassalanter: {
-        name: "Ammalia Cassalanter",
-        link: "https://5e.tools/bestiary.html#ammalia%20cassalanter_wdh",
-        hp: "10d8",
-        dex: 2,
-        group: 'Character'
-    },
-    durnan: {
-        name: "Durnan",
-        link: "https://5e.tools/bestiary.html#durnan_wdh",
-        hp: "17d8+68",
-        dex: 2,
-        group: 'Character'
-    },
-    grumshar: {
-        name: "Grum'shar",
-        link: "https://5e.tools/bestiary.html#grum'shar_wdh",
-        hp: "2d8",
-        dex: 0,
-        group: 'Character'
-    },
-    nihiloor: {
-        name: "Nihiloor",
-        link: "https://5e.tools/bestiary.html#nihiloor_wdh",
-        hp: "13d8+13",
-        dex: 1,
-        group: 'Character'
-    },
-    yagra: {
-        name: "Yagra Stonefist",
-        link: "https://5e.tools/bestiary.html#thug_mm",
-        hp: "5d8+10",
-        dex: 0,
-        group: 'Character'
-    }
+    // humanoid
+    apprenticeWizard: { name: "Apprentice Wizard", hp: "2d8", dex: 0, group: 'Humanoid', link: "https://5e.tools/bestiary.html#apprentice%20wizard_vgm" },
+    bandit: { name: "Bandit", hp: "2d8+2", dex: 1, group: 'Humanoid', link: "https://5e.tools/bestiary.html#bandit_mm" },
+    deepGnome: { name: "Deep Gnome", hp: "3d6+6", dex: 2, group: 'Humanoid', link: "https://5e.tools/bestiary.html#deep%20gnome%20(svirfneblin)_mm" },
+    doppelganger: { name: "DoppelGanger", hp: "8d8+16", dex: 4, group: 'Humanoid', link: "https://5e.tools/bestiary.html#doppelganger_mm" },
+    drowGunslinger: { name: "Drow Gunslinger", hp: "13d8+26", dex: 4, group: 'Humanoid', link: "https://5e.tools/bestiary.html#drow%20gunslinger_wdh" },
+    duergar: { name: "Duergar", hp: "4d8+8", dex: 0, group: 'Humanoid', link: "https://5e.tools/bestiary.html#duergar_mm" },
+    guard: { name: "Guard", hp: "2d8+2", dex: 1, group: 'Humanoid', link: "https://5e.tools/bestiary.html#guard_mm" },
+    swashbuckler: { name: "Swashbuckler", hp: "12d8+12", dex: 4, group: 'Humanoid', link: "https://5e.tools/bestiary.html#swashbuckler_vgm" },
+    veteran: { name: "Veteran", hp: "9d8+18", dex: 1, group: 'Humanoid', link: "https://5e.tools/bestiary.html#veteran_mm" },
+
+    // monster
+    gazer: { name: "Gazer", hp: "3d4+6", dex: 3, group: 'Monster', link: "https://5e.tools/bestiary.html#gazer_vgm" },
+    goblin: { name: "Goblin", hp: "2d6", dex: 2, group: 'Monster', link: "https://5e.tools/bestiary.html#goblin_mm" },
+    grayOoze: { name: "Gray Ooze", hp: "3d8+9", dex: -2, group: 'Monster', link: "https://5e.tools/bestiary.html#gray%20ooze_mm" },
+    intellectDevourer: { name: "Intellect Devourer", hp: "6d4+5", dex: 2, group: 'Monster', link: "https://5e.tools/bestiary.html#intellect%20devourer_mm" },
+    kenku: { name: "Kenku", hp: "3d8", dex: 3, group: 'Monster', link: "https://5e.tools/bestiary.html#kenku_mm" },
+    troll: { name: "Troll", hp: "8d10+40", dex: 1, group: 'Monster', link: "https://5e.tools/bestiary.html#troll_mm" },
+    wererat: { name: "Wererat", hp: "6d8+6", dex: 2, group: 'Monster', link: "https://5e.tools/bestiary.html#wererat_mm" },
+    mimic: { name: "Mimic", hp: "9d8+18", dex: 1, group: 'Monster', link: "https://5e.tools/bestiary.html#mimic_mm" },
+    minotaur: { name: "Minotaur", hp: "9d10+27", dex: 0, group: 'Monster', link: "https://5e.tools/bestiary.html#minotaur_mm" },
+    willowisp: { name: "Will-o'-Wisp", hp: "9d4", dex: 9, group: 'Monster', link: "https://5e.tools/bestiary.html#will-o'-wisp_mm" },
+    skeleton: { name: "Skeleton", hp: "2d8+4", dex: 2, group: 'Monster', link: "https://5e.tools/bestiary.html#skeleton_mm" },
+    shadow: { name: "Shadow", hp: "3d8+3", dex: 2, group: 'Monster', link: "https://5e.tools/bestiary.html#shadow_mm" },
+    boggle: { name: "Boggle", hp: "4d6+4", dex: 4, group: 'Monster', link: "https://5e.tools/bestiary.html#boggle_vgm" },
+    magmin: { name: "Magmin", hp: "2d6+2", dex: 2, group: 'Monster', link: "https://5e.tools/bestiary.html#magmin_mm" },
+    specter: { name: "Specter", hp: "5d8", dex: 2, group: 'Monster', link: "https://5e.tools/bestiary.html#specter_mm" },
+    smokeMephit: { name: "Smoke Mephit", hp: "5d6+5", dex: 2, group: 'Monster', link: "https://5e.tools/bestiary.html#smoke%20mephit_mm" },
+    
+    // beast
+    rat: { name: "Rat", hp: "1d4-1", dex: 0, group: 'Beast', link: "https://5e.tools/bestiary.html#rat_mm" },
+    stirges: { name: "Stirge", hp: "1d4", dex: 3, group: 'Beast', link: "https://5e.tools/bestiary.html#stirge_mm" },
+
+    // construct
+    animatedSword: { name: "AnimatedSword", hp: "5d6", dex: 2, group: 'Construct', link: "https://5e.tools/bestiary.html#flying%20sword_mm" },
+
+    // characters
+    victoroCassalanter: { name: "Victoro Cassalanter", hp: "15d8+30", dex: 1, group: 'Character', link: "https://5e.tools/bestiary.html#victoro%20cassalanter_wdh" },
+    ammaliaCassalanter: { name: "Ammalia Cassalanter", hp: "10d8", dex: 2, group: 'Character', link: "https://5e.tools/bestiary.html#ammalia%20cassalanter_wdh" },
+    durnan: { name: "Durnan", hp: "17d8+68", dex: 2, group: 'Character', link: "https://5e.tools/bestiary.html#durnan_wdh" },
+    grumshar: { name: "Grum'shar", hp: "2d8", dex: 0, group: 'Character', link: "https://5e.tools/bestiary.html#grum'shar_wdh" },
+    nihiloor: { name: "Nihiloor", hp: "13d8+13", dex: 1, group: 'Character', link: "https://5e.tools/bestiary.html#nihiloor_wdh" },
+    yagra: { name: "Yagra Stonefist", hp: "5d8+10", dex: 0, group: 'Character', link: "https://5e.tools/bestiary.html#thug_mm" }
 };
